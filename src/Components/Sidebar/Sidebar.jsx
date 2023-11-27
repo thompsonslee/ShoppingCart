e3988397b687dd4c5a6263564cee5f5af8965e58
thompsonslee <thompson.slee@hotmail.co.uk>
2023-11-27 14:31:38 +1100

    add icons to sidebar

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,19 +1,38 @@
 import './Sidebar.css'
+import homeLogo from '../../icons/house-solid.svg'
+import shopLogo from '../../icons/shop-solid.svg'
+import cartLogo from '../../icons/cart-shopping-solid.svg'
 import { Link } from "react-router-dom";
 
 
 export default function SideBar(){
     return(
-        <ul>
-            <li>
-                <Link to= '/'>Home</Link>
-            </li>
-            <li>
-                <Link to= 'shopPage'>Shop</Link>
-            </li>
-            <li>
-                <Link to = 'cartPage'>Cart</Link>
-            </li>
-        </ul>
+        <div className='sidebar'>
+            <h1>Lorem Ipsum</h1>
+            <ul className='main-list'>
+                <li>
+                    <Link to= '/'>
+                        <img src={homeLogo} className='icon'></img>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to= 'shopPage'>
+                        <img src={shopLogo} className='icon'></img>
+                        Shop
+                    </Link>
+                </li>
+                <li>
+                    <Link to = 'cartPage'>
+                        <img src={cartLogo} className='icon'></img>
+                        Cart
+                    </Link>
+                </li>
+            </ul>
+            <ul>
+                <li>Help</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
     )
 }
