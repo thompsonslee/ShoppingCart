6da88e53c5d0e6140a9e9781a39a6b968d21802d
thompsonslee <thompson.slee@hotmail.co.uk>
2023-12-05 14:18:56 +1100

    added styling to ProductPage

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -1,4 +1,4 @@
-
+import './ProductPage.css'
 import { useParams } from "react-router-dom"
 
 export default function ProductPage({getProduct,addToCart}){
@@ -6,9 +6,24 @@ export default function ProductPage({getProduct,addToCart}){
     const product = getProduct(id)
 
     return(
-        <>
-            <div>{product?.title}</div>
-            <button onClick={() => addToCart(id)}/>
-        </>
+        <div className="ProductPage">
+        {(product != undefined) && (
+            <>
+                <div className="product-image-container">
+                    <img src={product.image} />
+                </div>
+                <div className="product-heading">
+                    <h1>{product.title}</h1>
+                    <h2>{product.price}</h2>
+                    <button onClick={() => addToCart(id)}>ADD TO CART</button>
+                </div>
+                <div className="product-description Section">
+                    <h2>Description</h2>
+                    <p>{product.description}</p>
+                </div>
+                
+            </>
+        )}
+        </div>
     )
 }
